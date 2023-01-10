4fb712adb048b3dbfe29ee53ddd1c1c31b5db1d3
RaviShankar <ravishankar@Ravi-MacBook-Air-2.local>
2023-01-10 16:09:32 +0530

    added

--- setup.ps1
+++ setup.ps1
@@ -1,5 +1,5 @@
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls, [Net.SecurityProtocolType]::Tls11, [Net.SecurityProtocolType]::Tls12, [Net.SecurityProtocolType]::Ssl3
 [Net.ServicePointManager]::SecurityProtocol = "Tls, Tls11, Tls12, Ssl3"
-New-Item -Path 'C:\Users\adminuser' -ItemType Directory
-Invoke-WebRequest -Uri "https://tallymirror.tallysolutions.com/download_centre/Rel_2.1/TP/Full/setup.exe" -OutFile "C:\Users\adminuser\setup.exe"
+New-Item -Path 'C:\Users\adminuser\Downloads' -ItemType Directory
+Invoke-WebRequest -Uri "https://tallymirror.tallysolutions.com/download_centre/Rel_2.1/TP/Full/setup.exe" -OutFile "C:\Users\adminuser\Downloads\setup.exe"
 New-Item -Path 'C:\Users\adminuser\Downloads\test_folder' -ItemType Directory
