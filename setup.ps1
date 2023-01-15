dad89bfb93fa4cd320b917b7ae4b5922901518d9
ravibootlabs <109270469+ravibootlabs@users.noreply.github.com>
2023-01-15 17:28:26 +0530

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -1,2 +1,1 @@
-New-Item -Path 'C:\Users\adminuser\Downloads\test_folder' -ItemType Directory
-Invoke-WebRequest -Uri "https://tallymirror.tallysolutions.com/download_centre/Rel_2.1/TP/Full/setup.exe" -OutFile "C:\Users\setup.exe"
+Invoke-WebRequest -Uri "https://tallymirror.tallysolutions.com/download_centre/Rel_2.1/TP/Full/setup.exe" -OutFile "C:\Users\adminuser\Downloads\setup.exe" 
